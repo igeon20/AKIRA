b51e524afb3cb3834efe277d3688b76508375589
mikudaisuki <129954932+igeon20@users.noreply.github.com>
2025-07-04 22:36:50 +0900

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,25 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import ReactDOM from 'react-dom';
+import BotControl from './BotControl';
+import BotStatus from './BotStatus';
+import TradeLogs from './TradeLogs';
+import BalanceStatus from './BalanceStatus';
+import './App.css';
 
-// React의 진입점 (Entry point)
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const App = () => {
+  const [dummy, setDummy] = React.useState(0);
+  const reload = () => setDummy(prev => prev + 1);
+
+  return (
+    <div className="app-container">
+      <h1>Binance Trading Bot</h1>
+      <BotControl onStatusChange={reload} />
+      <BotStatus key={dummy} />
+      <BalanceStatus />
+      <TradeLogs key={dummy} />
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
